46d550bc91a5ac6e3017f1922b386bf9cd0428ef
sebastian.riedel <sebastian.riedel@38de3e8f-324a-0410-ba00-61a68ec87b4e>
2008-05-29 18:58:44 +0000

    * resizing of corpus loader and navigator now possible (after fixing they layout manager settings)

--- branches/0.0.x/src/whatswrong/CorpusNavigator.java
+++ branches/0.0.x/src/whatswrong/CorpusNavigator.java
@@ -203,7 +203,9 @@ public class CorpusNavigator extends JPanel implements CorpusLoader.Listener {
 
 
     add(searchPanel, new SimpleGridBagConstraints(0, 0, 2, 1));
-    add(new JScrollPane(results), new SimpleGridBagConstraints(0, 1, 2, 1));
+    JScrollPane resultsPane = new JScrollPane(results);
+    resultsPane.setMinimumSize(new Dimension(100,10));
+    add(resultsPane, new SimpleGridBagConstraints(0, 1, 2, 2));
 
     //setPreferredSize((new Dimension(100, (int) getPreferredSize().getHeight())));
     analyzer = new WhitespaceAnalyzer();
